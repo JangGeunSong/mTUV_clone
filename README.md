71c54a973c87f44fb58c1f7ad72d493964ac19db
JangGeunSong <sjg1468@gmail.com>
2019-04-11 20:34:09 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is DIP project repository
---
+++ mYUV/CYuvConfDlg.cpp
@@ -0,0 +1,34 @@
+﻿// CYuvConfDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "mYUV.h"
+#include "CYuvConfDlg.h"
+#include "afxdialogex.h"
+
+
+// CYuvConfDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CYuvConfDlg, CDialogEx)
+
+CYuvConfDlg::CYuvConfDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CYuvConfDlg::~CYuvConfDlg()
+{
+}
+
+void CYuvConfDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CYuvConfDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CYuvConfDlg 메시지 처리기
